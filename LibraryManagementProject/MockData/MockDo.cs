45b928b4afa7854b8c4e88f3b46643068f1a89bb
bhawnarathor1204 <brathor@sebonetechnologies.com>
2022-10-08 12:58:04 +0530

    Revert "Adding new lines for check revert"
    This reverts commit 8a541a313f911ce687810263ffb3d558abad0ebf.

--- LibraryManagementProject/MockData/MockDo.cs
+++ LibraryManagementProject/MockData/MockDo.cs
@@ -11,9 +11,6 @@ namespace LibraryManagementProject.MockData
     {
         public List<BookResponseDo> getMockData()
         {
-
-
-
             List<BookResponseDo> bookList = new List<BookResponseDo>();
             bookList.Add(new BookResponseDo(101, "The great gatsBy", "henry", new DateTime(2022,03,10), new DateTime(2022,03,15), 123456, "return", "Fiction"));
             bookList.Add(new BookResponseDo(102, "Henry Miller", "andy", new DateTime(2022,04,10 ), new DateTime(2022,05,15), 87874838, "Not return", "NonFiction"));
@@ -25,19 +22,6 @@ namespace LibraryManagementProject.MockData
             bookList.Add(new BookResponseDo(108, "The Help", "leo tolstoy", new DateTime(2022, 04, 10), new DateTime(2022, 05, 15), 897859, "Non return", "Journal"));
             bookList.Add(new BookResponseDo(109, "Maus", "franz kafza", new DateTime(2022, 03, 10), new DateTime(2022, 03, 15), 6356216, "return", "Fiction"));
             bookList.Add(new BookResponseDo(110, "War and Peas", "cormac", new DateTime(2022, 04, 10), new DateTime(2022, 05, 15), 6476318, "Non return", "NonFiction"));
-
-
-            // This lines add for checking revert is correctly working or not
-            bookList.Add(new BookResponseDo(201, "The great gatsBy", "henry", new DateTime(2022,03,10), new DateTime(2022,03,15), 123456, "return", "Fiction"));
-            bookList.Add(new BookResponseDo(202, "Henry Miller", "andy", new DateTime(2022,04,10 ), new DateTime(2022,05,15), 87874838, "Not return", "NonFiction"));
-            bookList.Add(new BookResponseDo(203, "Sarah water", "waltous", new DateTime(2022, 04, 15), new DateTime(2022, 04, 18), 964728, "return", "Historical"));
-            bookList.Add(new BookResponseDo(204, "The vagetarian", "milan kudera", new DateTime(2022, 05, 15), new DateTime(2022, 05, 15), 7847984, "Non return", "Journal"));
-            bookList.Add(new BookResponseDo(205, "All the light we can not see", "stifen king", new DateTime(2022, 04, 15), new DateTime(2022, 04, 18), 7463764, "return", "Fiction"));
-            bookList.Add(new BookResponseDo(206, "Animal farm", "arthur golden", new DateTime(2022, 04, 10), new DateTime(2022, 05, 15), 344355, "Non return", "NonFiction"));
-            bookList.Add(new BookResponseDo(207, "The bell jar", "ernest emigy", new DateTime(2022, 03, 10), new DateTime(2022, 03, 15), 746327, "return", "Historical"));
-            bookList.Add(new BookResponseDo(208, "The Help", "leo tolstoy", new DateTime(2022, 04, 10), new DateTime(2022, 05, 15), 897859, "Non return", "Journal"));
-            bookList.Add(new BookResponseDo(209, "Maus", "franz kafza", new DateTime(2022, 03, 10), new DateTime(2022, 03, 15), 6356216, "return", "Fiction"));
-            bookList.Add(new BookResponseDo(220, "War and Peas", "cormac", new DateTime(2022, 04, 10), new DateTime(2022, 05, 15), 6476318, "Non return", "NonFiction"));
 
             return bookList;
         }
